88dc7dcc9761e6e65f9cbda99c74d11a9ef50046
appleboblin <appleboblin@proton.me>
2024-06-06 18:14:55 +0800

    update flake to include node, removed shell.nix.

--- flake.nix
+++ flake.nix
@@ -1,17 +1,34 @@
 {
-    description = "Work please";
+    description = "Finance application project in python, node required for database editing";
 
     inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     };
 
-    outputs = { self, nixpkgs, flake-utils }:
-        flake-utils.lib.eachDefaultSystem
-        (system:
-            let pkgs = nixpkgs.legacyPackages.${system}; in
-            {
-            devShells.default = import ./shell.nix { inherit pkgs; };
-            }
-        );
+    outputs = { self , nixpkgs ,... }: let
+        system = "x86_64-linux";
+        # system = "x86_64-darwin";
+    in {
+        devShells."${system}".default = let
+        pkgs = import nixpkgs {
+            inherit system;
+        };
+        in pkgs.mkShell {
+            # create an environment with both Node.js and Python
+            packages = with pkgs; [
+            nodejs_20
+            nodePackages.npm
+            sqlite
+            (python312.withPackages (pythonPkgs: with pythonPkgs; [
+                tkinter
+            ]))
+            ];
+
+            shellHook = ''
+            echo "node `${pkgs.nodejs}/bin/node --version`"
+            venv="$(cd $(dirname $(which python)); cd ..; pwd)"
+            ln -Tsf "$venv" .venv
+        '';
+        };
+    };
 }
